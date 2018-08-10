ec7122e8fe3fbb7e94a652e61e9d0868975e06e2
lisainterfacehurley <30816052+lisainterfacehurley@users.noreply.github.com>
2018-08-09 22:37:21 -0500

    Adds Log In JS
    Adds Log In JS

--- GifLibrary/js/libraryLogIn.js
+++ GifLibrary/js/libraryLogIn.js
@@ -1,5 +1,5 @@
-//Users that will be seen if someone enters an email and password correctly
-const USERS = [
+// Declare Users     Users that will be seen if someone enters an email and password correctly
+var users = [
       {email: "steveg@example.com", 
        password: "PaSsWoRd", 
        birthday: "2000-09-30 00:00:00",
@@ -19,14 +19,65 @@ const USERS = [
       {email: "draganP@example.hr", 
        password: "volimbaku", 
        birthday: "1995-06-06 00:00:00",
-       role: "advanced"}]
-// Accesses Local Storage object and adds a data item into it using Storage.setItem();
-localStorage.setItem(user, userObject);
+       role: "advanced"}];
 
-//Add a click Listener to the submit button that will fire a function called logIn()
-document.getElementById("logIn").addEventListener.onclick("logIn");
+// Save References to the Password and email fields
+var emailInput = $("#email");
+var passwordInput = $("#password");
 
-//Log In function should see if a user with email exists in Users constant   I still do not know how to code this filter section
+// Add an Event Listener to the submit button that will fire a function called logIn()
+button.addEventListener ("click", function() {
+  alert("Logged In Successfully!");
+});
+
+const searchUserElement = document.querySelector('#js-search-query');
+const searchItemsButton  = document.querySelector('#js-filter-items');
+const searchableItems    = document.querySelectorAll('.searchable');
+const sortBy             = document.querySelector('#js-sort-by');
+const listContainer      = document.querySelector('#js-list');
+
+// Search button callback
+searchItemsButton.addEventListener('click', filterItems);
+
+// function for filtering the list items and displaying the correct items
+function filterItems() {
+  let searchParam = searchInputElement.value.toLowerCase();
+  searchableItems.forEach((listItem) => {
+    let itemName = listItem.dataset.name.toLowerCase();
+    if (-1 == itemName.indexOf(searchParam)) {
+      listItem.classList.add('hidden');
+    } else {
+      listItem.classList.remove('hidden');
+    }
+  });
+}
+
+// Callback for sorting the items
+sortBy.addEventListener('change', function(event) {
+  // Need to convert to an array, since we can't call sort on a NodeList
+  itemsArray = Array.prototype.slice.call(searchableItems, 0);
+  // Sort the array of nodes
+  itemsArray = itemsArray.sort(function(a, b) {
+    let aVal = ('name' == sortBy.value) ? a.dataset.name.toLowerCase() : a.dataset.index;
+    let bVal = ('name' == sortBy.value) ? b.dataset.name.toLowerCase() : b.dataset.index;
+    return aVal > bVal;
+  });
+  // Empty the list of items and re-add everything in the sorted order
+  listContainer.innerHTML = ''
+  itemsArray.forEach((item, index) => {
+    listContainer.append(item);
+  });
+});
+
+//Log In function should see if a user with email exists in Users constant   
+function validate(){
+var userEmail = document.getElementById("email").value;
+var userPassword = document.getElementById("password").value;
+if ( userEmail == "" && userPassword == ""){
+alert ("Login Successfully");
+window.location = "libraryPage.html"; // Redirecting to Library Page.
+return false;
+}
 var user = user.filter;
 
 userPassword = user.val;
@@ -36,72 +87,25 @@ if userEmail && userPassword{
 }
 
 // After checking both email and password,  If a user with that email exists, check to see if the case sensitive password matches one for user.
-
 $(document).ready(function(){
    $("#textNewEmail, #textConfirmEmail").keyup(checkEmailMatch);
     return user.email.toLowerCase == .toLowerCase;
 });
-
 
 // If the user's email is authenticated and true, the user will be automatically sent to the Library Page
 if(logInEmail.getAttribute("authenticated")!=null && logInEmail.getAttribute("authenticated").equals(true)){
    return("./libraryPage.html");
 }
 
-//if errors, add error to errorContainer
+// If errors, add error to errorContainer
 else{
     errorContainer.append("Email or Password is incorrect!");
 }
 
 // Clear all Items in Error Container
 errorContainer.clear();
-
-localStorage.clear();
-
-
-
-
-
-
-
-/* save Data Values for email, password, logIn, errorContainer 
-localStorage.setItem("email", "password", "logIn", "errorContainer");
-
-retrieve Data Values for email, password, logIn, errorContainer 
-var logInData = localStorage.getItem("logInData");
-
-
-
-/*window.onload = function(){
-    $("#showPassword").hide();
-}
-
-$("#textPassword").on('change',function(){  
-		if($("#textPassword").val()){
-			$("#showPassword").show();
-		}
-		else{
-			$("#showPassword").hide();
-		}
-});
-
-$(".reveal").on('click',function(){
-    var $pwd = $("#textPassword");
-    if ($pwd.attr('type') === 'password'){
-        $pwd.attr('type', 'text');
-    } 
-		else{
-        $pwd.attr('type', 'password');
-    }
-});
-*/
-
-
-
-
-
-
-
-
+    
+// Accesses Local Storage object and adds a data item into it using localStorage.setItem();
+`localStorage.setItem('user', filteredUser[0])`;
 
 
